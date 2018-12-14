5d1c81bf80b974cc384ae8f26272c26fdf3003d4
Harika-Raj <r121662@rguktrkv.ac.in>
2018-12-15 01:03:54 +0530

    Add the FlatList to list out the names

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, FlatList , View} from 'react-native';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+            this.state ={
+                FlatListItem:[
+                    {key: 'Ranga'},
+                    {key: 'Harika'},
+                    {key: 'Manoj'},
+                    {key: 'Harish'},
+                    {key: 'Hema'},
+                    {key: 'Katta'},
+                    {key: 'Lokesh'}
+                ],
+            };
+        }
+        ListEmpty = () => {
+            return (
+                <View style={styles.MainContainer}>
+                    <Text style={{ textAlign: 'center' }}>No Data Found</Text>
+                </View>
+            );
+        };
   render() {
       return (
           <View style={styles.mainContainer}>
               <View style={styles.headerContainer}>
                   <View style={styles.leftHeaderContainer}>
-                      <Text style={styles.logoText}>WhatsApp</Text>
+                      <Text style={styles.logoText}>P2PAPP</Text>
                   </View>
                   <View style={styles.rightHeaderContainer}>
                   </View>
               </View>
               <View style={styles.contentContainer}>
+                  <FlatList
+                      data={this.state.FlatListItem}
+                      renderItem={({ item }) => (
+                          <Text style={styles.item}> {item.key} </Text>
+                      )}
+                      ListEmptyComponent={this.ListEmpty}/>
               </View>
           </View>
       );
@@ -23,6 +50,12 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F5FCFF',
         height: 24
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        flexDirection: "row",
     },
     headerContainer: {
         flex: 1,
@@ -42,6 +75,7 @@ const styles = StyleSheet.create({
     },
     contentContainer: {
         flex: 6,
+        flexDirection: "row",
     },
     logoText: {
         color: "white",
