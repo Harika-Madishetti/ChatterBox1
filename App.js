62fbebe4118302861b13f10f238b69e328e5ab9c
Harika-Raj <r121662@rguktrkv.ac.in>
2018-12-14 16:16:24 +0530

    Add header to the application

--- App.js
+++ App.js
@@ -1,31 +1,53 @@
-
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-          <Text>Hello world</Text>
-      </View>
-    );
+      return (
+          <View style={styles.mainContainer}>
+              <View style={styles.headerContainer}>
+                  <View style={styles.leftHeaderContainer}>
+                      <Text style={styles.logoText}>WhatsApp</Text>
+                  </View>
+                  <View style={styles.rightHeaderContainer}>
+                  </View>
+              </View>
+              <View style={styles.contentContainer}>
+              </View>
+          </View>
+      );
   }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#F5FCFF',
+        height: 24
+    },
+    headerContainer: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "#075e54",
+        alignItems:"center",
+        paddingRight: 5
+    },
+    leftHeaderContainer: {
+        alignItems: "flex-start",
+        flexDirection: "row"
+    },
+    rightHeaderContainer: {
+        alignItems: "flex-end",
+        flexDirection: "row"
+    },
+    contentContainer: {
+        flex: 6,
+    },
+    logoText: {
+        color: "white",
+        fontWeight: "bold",
+        fontSize: 16,
+        alignItems: "flex-start",
+        marginLeft: 10
+    },
 });
